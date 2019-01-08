26423b15e26082bf4d574e9fd184fbeff503b5ed
oriprice <oripr@wix.com>
2019-01-08 20:44:27 +0200

    added config icon in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import { shape, string } from 'prop-types';
+import classnames from 'classnames';
 
 import SearchInput from './components/SearchInput/SearchInput';
 import Options from './components/Options/Options';
+import styles from './App.scss';
 
-const App = ({ location }) => (location.search.indexOf('options') > 0 ? (<Options />) : (<SearchInput />));
+const App = ({ location }) => (location.search.indexOf('options') > 0 ? <Options />
+  : (
+    <div>
+      <a href="?options" target="_blank"><i className={classnames(styles.icon, 'fas fa-cog')} /></a>
+      <SearchInput />
+    </div>
+  )
+);
 
 App.propTypes = {
   location: shape({
