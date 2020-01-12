2381d19915f80406a6708055568be0b202cfb2e0
oriprice <oripr@wix.com>
2020-01-12 10:10:55 +0200

    Clear cached results on upgrade from version < 2.0.7

--- src/background.js
+++ src/background.js
@@ -1,8 +1,7 @@
 chrome.runtime.onInstalled.addListener(({ reason, previousVersion }) => {
   if (reason === 'update') {
-    const versionArray = previousVersion.split('.').map((digit) => parseInt(digit, 10));
-    if (versionArray[0] === 1
-      || (versionArray[0] === 2 && versionArray[1] === 0 && versionArray[2] < 7)) {
+    const version = parseInt(previousVersion.replace(/\./g, ''), 10);
+    if (version < 207) {
       window.chrome.storage.sync.set({ options: {} });
     }
   }
